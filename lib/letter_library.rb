27f6ea8c0d862df8b2e2200bca0b5f6af2e07e05
cooljasonmelton <cooljasonmelton>
2020-07-19 18:51:36 -0500

    library thru g

--- lib/letter_library.rb
+++ lib/letter_library.rb
@@ -60,91 +60,98 @@ letter_c6 = <<-c
 c
 
 letter_c = [letter_c1, letter_c2, letter_c3, letter_c4,letter_c5, letter_c6]
-p c
 
-letter_b1 = <<-B
+letter_d1 = <<-d
 ██████╗░
-B
-letter_b2 = <<-B
+d
+letter_d2 = <<-d
 ██╔══██╗
-B
-letter_b3 = <<-B
-██████╦╝
-B
-letter_b4 = <<-B
-██╔══██╗
-B
-letter_b5 = <<-B
-██████╦╝
-B
-letter_b6 = <<-B
+d
+letter_d3 = <<-d
+██║░░██║
+d
+letter_d4 = <<-d
+██║░░██║
+d
+letter_d5 = <<-d
+██████╔╝
+d
+letter_d6 = <<-d
 ╚═════╝░
-B
+d
 
-letter_b = [letter_b1, letter_b2, letter_b3, letter_b4,letter_b5, letter_b6]
+letter_d = [letter_d1, letter_d2, letter_d3, letter_d4,letter_d5, letter_d6]
 
-letter_b1 = <<-B
-██████╗░
-B
-letter_b2 = <<-B
-██╔══██╗
-B
-letter_b3 = <<-B
-██████╦╝
-B
-letter_b4 = <<-B
-██╔══██╗
-B
-letter_b5 = <<-B
-██████╦╝
-B
-letter_b6 = <<-B
-╚═════╝░
-B
+letter_e1 = <<-e
+███████╗
+e
+letter_e2 = <<-e
+██╔════╝
+e
+letter_e3 = <<-e
+█████╗░░
+e
+letter_e4 = <<-e
+██╔══╝░░
+e
+letter_e5 = <<-e
+███████╗
+e
+letter_e6 = <<-e
+╚══════╝
+e
 
-letter_b = [letter_b1, letter_b2, letter_b3, letter_b4,letter_b5, letter_b6]
+letter_e = [letter_e1, letter_e2, letter_e3, letter_e4,letter_e5, letter_e6]
 
-letter_b1 = <<-B
-██████╗░
-B
-letter_b2 = <<-B
-██╔══██╗
-B
-letter_b3 = <<-B
-██████╦╝
-B
-letter_b4 = <<-B
-██╔══██╗
-B
-letter_b5 = <<-B
-██████╦╝
-B
-letter_b6 = <<-B
-╚═════╝░
-B
+letter_f1 = <<-f
+███████╗
+f
+letter_f2 = <<-f
+██╔════╝
+f
+letter_f3 = <<-f
+█████╗░░
+f
+letter_f4 = <<-f
+██╔══╝░░
+f
+letter_f5 = <<-f
+██║░░░░░
+f
+letter_f6 = <<-f
+╚═╝░░░░░
+f
 
-letter_b = [letter_b1, letter_b2, letter_b3, letter_b4,letter_b5, letter_b6]
+letter_f = [letter_f1, letter_f2, letter_f3, letter_f4,letter_f5, letter_f6]
 
-letter_b1 = <<-B
-██████╗░
-B
-letter_b2 = <<-B
-██╔══██╗
-B
-letter_b3 = <<-B
-██████╦╝
-B
-letter_b4 = <<-B
-██╔══██╗
-B
-letter_b5 = <<-B
-██████╦╝
-B
-letter_b6 = <<-B
-╚═════╝░
-B
+letter_g1 = <<-g
+░██████╗░
+g
+letter_g2 = <<-g
+██╔════╝░
+g
+letter_g3 = <<-g
+██║░░██╗░
+g
+letter_g4 = <<-g
+██║░░╚██╗
+g
+letter_g5 = <<-g
+╚██████╔╝
+g
+letter_g6 = <<-g
+░╚═════╝░
+g
 
-letter_b = [letter_b1, letter_b2, letter_b3, letter_b4,letter_b5, letter_b6]
+letter_g = [letter_g1, letter_g2, letter_g3, letter_g4,letter_g5, letter_g6]
+letter_g.each{ |g| puts g.chomp}
+
+
+
+
+
+
+
 
 letter_b1 = <<-B
 ██████╗░
